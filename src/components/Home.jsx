a90ab291d96a498e9d0e68b24d078232405e598a
David <cordoba_d@rocketmail.com>
2024-08-04 20:10:55 -0300

    f

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,12 @@ const Home = () => {
   return (
     <div className='min-h-[64vh]  flex flex-row justify-between items-center lg:px-32 px-5 lg:bg-contain bg-contain bg-center  bg-no-repeat sm:bg-[url("./assets/hero.jpeg")]'>
         <div className='w-full lg:w-2/3 space-y-5'>
-            <h1 className='text-amber-400 font-semibold text-5xl'>Elevate uoy inne foodie with every bite</h1>
-            <p className='text-backgroundColor'>
+            <h1 className='text-gray-400 font-semibold text-5xl'>Lorem ipsum dolor sit amet.</h1>
+            <p className='text-orange-600 font-semibold'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum corrupti id soluta porro alias provident.
             </p>
             <div>
-                <Button title='Ordena ahora wachin'/>
+                <Button title='Ordena ahora'/>
             </div>
         </div>
     </div>
